fb4081c4ed6d57b1e95dffefd10ba45426ac1dbd
tabsheermk <creativemk12@gmail.com>
2023-08-31 14:24:09 +0530

    7-Initial Modal Added For Server Creation

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
   const profile = await initialProfile();
@@ -18,7 +19,7 @@ const SetupPage = async () => {
     return redirect(`/channels/${server.id}`);
   }
 
-  return <div>Create A server</div>;
+  return <InitialModal />;
 };
 
 export default SetupPage;
