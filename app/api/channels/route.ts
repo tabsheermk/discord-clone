82e37335eddab09f9d2b067b7e58852cbe96b607
tabsheermk <creativemk12@gmail.com>
2023-09-01 13:56:58 +0530

    19-Server Channel List & Member List added

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -44,6 +44,8 @@ export async function POST(req: Request) {
         },
       },
     });
+
+    return NextResponse.json(server);
   } catch (error) {
     console.log("CHANNELS_POST", error);
     return new NextResponse("Internal Error", { status: 500 });
