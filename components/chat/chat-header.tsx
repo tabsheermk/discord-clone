d64c77c3fd3f7e965c3db9d88b2a217b1ada9160
tabsheermk <creativemk12@gmail.com>
2023-09-02 18:55:29 +0530

    36-Livekit connected, audio & video channels created, DM video call added

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import { MobileToggle } from "../mobile-toggle";
 import { channel } from "diagnostics_channel";
 import { UserAvatar } from "../user-avatar";
 import { SocketIndicator } from "../socket-indicator";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -28,6 +29,7 @@ export const ChatHeader = ({
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
+        {type === "conversation" && <ChatVideoButton />}
         <SocketIndicator />
       </div>
     </div>
